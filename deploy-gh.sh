0989c4efe26a516cf44818b73ac5be0c6fe62c70
RojkovD <rojkov.dmt@gmail.com>
2021-10-17 00:57:55 +0700

    deploy-gh update commit message added

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -8,6 +8,6 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
 && rm -rf static \
 && mv build/* ./ \
 && git add static/* index.html favicon.ico asset-manifest.json logo192.png logo512.png manifest.json robots.txt \
-&& git commit \
+&& git commit -m "updated github pages" \
 && git push github gh-pages \
 && checkout $CURRENT_BRANCH
