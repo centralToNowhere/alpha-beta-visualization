17d555a41bb1ef5057864cd34068b6b7c0f8d2dc
RojkovD <rojkov.dmt@gmail.com>
2021-10-17 00:46:27 +0700

    deploy-gh error fixed

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -5,7 +5,9 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
 && git checkout gh-pages \
 && git merge develop \
 && npm run build \
+&& rm -rf static \
 && mv build/* ./ \
+&& git add static/* index.html favicon.ico asset-manifest.json logo192.png logo512.png manifest.json robots.txt \
 && git commit \
 && git push github gh-pages \
 && checkout $CURRENT_BRANCH
